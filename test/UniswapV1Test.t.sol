5841125b8ac99370d7c93dcb68b3781864f1f6de
Sean Emile <seanemile.se@protonmail.com>
2023-04-29 20:13:15 +0300

    Added factory tests

--- test/UniswapV1Test.t.sol
+++ test/UniswapV1Test.t.sol
@@ -8,4 +8,18 @@ import "./interfaces/Factory.sol";
 
 contract UniswapV1Test is Test {
     Factory public factory = Factory(0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95);
+    uint256 public mainnetFork;
+    address public tether = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+
+    string public MAINNET_RPC_URL = vm.envString("MAINNET_RPC_URL");
+
+    function setUp() public {
+        mainnetFork = vm.createFork(MAINNET_RPC_URL);
+        vm.selectFork(mainnetFork);
+        vm.rollFork(17_152_296);
+    }
+
+    function test_getExchange() public {
+        assertEq(factory.getExchange(tether), 0xc8313c965C47D1E0B5cDCD757B210356AD0e400C);
+    }
 }
