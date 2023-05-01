ea010013856cb0e911f94d5bbe4813685048e191
Sean Emile <seanemile.se@protonmail.com>
2023-05-01 23:02:14 +0300

    Updated test to use fork programatically

--- test/UniswapV1Test.t.sol
+++ test/UniswapV1Test.t.sol
@@ -11,7 +11,13 @@ contract UniswapV1Test is Test {
     uint256 public mainnetFork;
     address public tether = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
 
-    function setUp() public {}
+    string public MAINNET_RPC_URL = vm.envString("MAINNET_RPC_URL");
+
+    function setUp() public {
+        mainnetFork = vm.createFork(MAINNET_RPC_URL);
+        vm.selectFork(mainnetFork);
+        vm.rollFork(17_152_296);
+    }
 
     function test_getExchange() public {
         assertEq(factory.getExchange(tether), 0xc8313c965C47D1E0B5cDCD757B210356AD0e400C);
